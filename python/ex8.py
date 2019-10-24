073653e3c8c087f636b565259b27420e3d35fd39
Tatyana S <tana@example.com>
2019-10-24 22:32:49 +0700

    Задание 8

---
+++ python/ex8.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def main():
+    driver = webdriver.Chrome()
+    try:
+        driver.get("http://localhost/litecart/")
+
+        wait = WebDriverWait(driver, 10) # seconds
+        wait.until(EC.title_contains("Online Store"))
+
+        picts = driver.find_elements_by_class_name("product.column.shadow.hover-light")
+
+        for pict in picts:
+            stickers = pict.find_elements_by_class_name("sticker")
+
+            if len(stickers) != 1:
+                raise Exception("Стикер не один")
+            else:
+                print ("Стикер один")
+
+    finally:
+        driver.close()
+
+
+if __name__ == "__main__":
+    main()
